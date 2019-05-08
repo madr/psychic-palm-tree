cb7fff322b9b79613ddba76f19cc452c7942a8ab
Anders Ytterström <anders.ytterstrom@kundo.se>
2019-05-08 17:07:41 +0200

    Use another port for local dev

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :risk_battle_simulator, RiskBattleSimulatorWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4099],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
