36e88421b3019de1732242791ebf53e5dec2e876
Anders Ytterström <anders.ytterstrom@kundo.se>
2019-05-28 15:05:34 +0200

    Move get_options to changeset

--- lib/risk_battle_simulator_web/models/simulation.ex
+++ lib/risk_battle_simulator_web/models/simulation.ex
@@ -18,4 +18,12 @@ defmodule Simulation do
     |> validate_number(:defending, greater_than: 0)
     |> validate_number(:battles, less_than_or_equal_to: 500, greater_than_or_equal_to: 100)
   end
+
+  def get_options(true, true), do: ["has_hero", "is_fort"]
+
+  def get_options(false, true), do: ["is_fort"]
+
+  def get_options(true, false), do: ["has_hero"]
+
+  def get_options(false, false), do: []
 end
