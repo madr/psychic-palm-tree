ab1af8f618d24b9f49a8fe5b9a7227fa5f3d551e
Anders Ytterström <anders.ytterstrom@kundo.se>
2019-05-28 15:07:22 +0200

    Validate data with Ecto in battle controller

--- lib/risk_battle_simulator_web/controllers/battle_controller.ex
+++ lib/risk_battle_simulator_web/controllers/battle_controller.ex
@@ -1,59 +1,31 @@
 defmodule RiskBattleSimulatorWeb.BattleController do
   use RiskBattleSimulatorWeb, :controller
 
-  @default_num 300
-  @default_attacking 3
-  @default_defending 2
+  import Ecto.Changeset
 
-  def simulation(conn, %{
-        "attacking" => attacking_troops,
-        "defending" => defending_troops,
-        "battles" => num,
-        "options" => options
-      }) do
-    results = response(attacking_troops, defending_troops, num, options)
-    render(conn, "battle.html", results)
+  def simulation(conn, params) do
+    simulation = %Simulation{}
+    changeset = Simulation.changeset(simulation, params)
+
+    case changeset.valid? do
+      true -> render(conn, "battle.html", response(changeset))
+    end
   end
 
-  def simulation(conn, %{
-        "attacking" => attacking_troops,
-        "defending" => defending_troops,
-        "battles" => num
-      }) do
-    results = response(attacking_troops, defending_troops, num)
-    render(conn, "battle.html", results)
-  end
-
-  def simulation(conn, %{
-      "attacking" => attacking_troops,
-      "defending" => defending_troops,
-      }) do
-    results = response(attacking_troops, defending_troops, Integer.to_string(@default_num))
-    render(conn, "battle.html", results)
-  end
-
-  def simulation(conn, _) do
-    render(conn, "battle.html", %{
-      result: false,
-      values: %{
-        :attacking => @default_attacking,
-        :defending => @default_defending,
-        :has_hero => false,
-        :is_fort => false,
-        :battles => @default_num
-      }
-    })
-  end
-
-  defp simulate([attacking_troops, defending_troops, num], options) do
+  defp simulate(attacking_troops, defending_troops, num, options) do
     RiskDice.simulate(attacking_troops, defending_troops, num, options)
   end
 
-  defp response(attacking_troops, defending_troops, num, options \\ %{}) do
-    result =
-      [attacking_troops, defending_troops, num]
-      |> Enum.map(&String.to_integer/1)
-      |> simulate(options)
+  defp response(%{
+        attacking: attacking_troops,
+        defending: defending_troops,
+        has_hero: has_hero,
+        is_fort: is_fort,
+        battles: num
+    }) do
+    options = Simulation.get_options(has_hero, is_fort)
+
+    result = simulate(attacking_troops, defending_troops, num, options)
 
     %{
       result: result,
@@ -63,6 +35,35 @@ defmodule RiskBattleSimulatorWeb.BattleController do
         :has_hero => "has_hero" in options,
         :is_fort => "is_fort" in options,
         :battles => num
+      }
+    }
+  end
+  defp response(changeset) do
+    data =
+      changeset
+      |> apply_changes()
+      |> Map.from_struct()
+
+    %{
+      __meta__: _meta,
+      has_hero: has_hero,
+      is_fort: is_fort,
+      attacking: attacking,
+      defending: defending,
+      battles: battles
+    } = data
+
+    options = Simulation.get_options(has_hero, is_fort)
+    result = RiskDice.simulate(attacking, defending, battles, options)
+
+    %{
+      result: result,
+      values: %{
+        :attacking => attacking,
+        :defending => defending,
+        :has_hero => "has_hero" in options,
+        :is_fort => "is_fort" in options,
+        :battles => battles
       }
     }
   end
